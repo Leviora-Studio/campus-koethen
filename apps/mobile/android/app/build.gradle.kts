5a651e1502ea74d1b3a460c6399420fbcb1a99e5
Erik Engler <erik@leviora.studio>
2026-08-28 00:48:04 +0200

    chore(mobile): configure store release identifiers

--- apps/mobile/android/app/build.gradle.kts
+++ apps/mobile/android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "dev.erikengler.campuskoethen"
+        applicationId = "erikengler.campuskoethen"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
